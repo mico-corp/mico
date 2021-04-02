29d70fa4bc4a31fa7e4bcd2846c876ad3c686b44
Bardo91 <19bardo91@gmail.com>
2021-04-02 19:33:31 +0200

    fix in opencv install script in unix

--- cmake/dependencies/unix_install_impl/installOpencv.sh
+++ cmake/dependencies/unix_install_impl/installOpencv.sh
@@ -19,22 +19,22 @@
 ##  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ##---------------------------------------------------------------------------------------------------------------------
 
-set build_directory=%1
-set install_directory=%2
+build_directory=%1
+install_directory=%2
 
-git clone https://github.com/opencv/opencv %build_directory%/opencv
-git clone https://github.com/opencv/opencv_contrib %build_directory%/opencv_contrib
+git clone https://github.com/opencv/opencv $build_directory/opencv
+git clone https://github.com/opencv/opencv_contrib $build_directory/opencv_contrib
 
-cd %build_directory%/opencv_contrib
+cd $build_directory/opencv_contrib
 git checkout 4.2.0
 
-cd %build_directory%/opencv
+cd $build_directory/opencv
 git checkout 4.2.0
 
 mkdir build
 cd build
 
-cmake .. -DCMAKE_INSTALL_PREFIX=%install_directory% -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF  -DOPENCV_EXTRA_MODULES_PATH=%build_directory%/opencv_contrib/modules
+cmake .. -DCMAKE_INSTALL_PREFIX=$install_directory -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF  -DOPENCV_EXTRA_MODULES_PATH=$build_directory/opencv_contrib/modules
 cmake --build . --config Release -j
 cmake --build . --config Release -j --target INSTALL
 cmake --build . --config debug -j
